11d853cee0ab326868e3fb9325a7cd866e711b80
Matthew Carter <m@ahungry.com>
2020-11-19 00:21:05 -0500

    Add some updates

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ahungry Cards
+
+Tinkering with a browser based card game/setup leveraging DOM/CSS/JS
+for basic primitives (drag and drop, double click etc.).
--- main.js
+++ main.js
@@ -1,16 +1,38 @@
+let mouseX = 0
+let mouseY = 0
 let x = 0
 let y = 0
 let z = 0
+
+document.addEventListener('mousedown', (e) => {
+  mouseX = e.clientX
+  mouseY = e.clientY
+})
 
 const els = document.querySelectorAll('.card')
 
 for (let i = 0; i < els.length; i++) {
   const el = els[i]
 
+  el.addEventListener('mousedown', (e) => {
+    const clickCount = el.clicked || 0
+    el.clicked = clickCount + 1
+
+    if (el.clicked >= 2) alert('activate it...')
+    setTimeout(() => { el.clicked = 0 }, 500)
+  })
+
+  el.addEventListener('dragstart', (e) => {
+    const x = e.target.offsetLeft
+    const y = e.target.offsetTop
+  })
+
   el.addEventListener('dragend', (e) => {
-    console.log('drag is ended, it is: ', x)
-    el.style.left = (x - 50) + 'px' // rect.left
-    el.style.top = (y - 50) + 'px' // rect.top
+    const offsetX = (mouseX - el.offsetLeft)
+    const offsetY = (mouseY - el.offsetTop)
+
+    el.style.left = x - offsetX + 'px' // rect.left
+    el.style.top = y - offsetY + 'px' // rect.top
     el.style.zIndex = z++
     el.oldX = 0
     el.oldY = 0
@@ -22,8 +44,6 @@ for (let i = 0; i < els.length; i++) {
     const oldY = el.oldY || 0
     const newX = e.clientX
     const newY = e.clientY
-
-    console.log('vals: ', { oldX, oldY, newX, newY })
 
     if (newX !== oldX) x = newX
     if (newY !== oldY) y = newY
